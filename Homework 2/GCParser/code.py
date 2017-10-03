7d2569b1e831126a3cfb4f2fadd353fc7ac15307
Mohammed Jasam <mnqnd@mst.edu>
2017-10-03 11:01:40 -0700

    Segregated the folders of Circuits, Results, Inputs

--- Homework 2/GCParser/code.py
+++ Homework 2/GCParser/code.py
@@ -7,21 +7,25 @@ cirName = input()
 #-------------------------------------------------------------------------------
 # Inputs into the Client and Server
 print("\nEnter the value for Client")
-with open("Client.txt", "w") as text_file:
+with open("./Inputs/InputClient.txt", "w") as text_file:
     b=int(input())
     print("a %d" %b, file=text_file)
+text_file.close()
 
 print("\nEnter the value for Server")
-with open("Server.txt", "w") as text_file:
+with open("./Inputs/InputServer.txt", "w") as text_file:
     a=int(input())
     print("b %d" %a, file=text_file)
+text_file.close()
 #-------------------------------------------------------------------------------
 # Command to check the Circuit file
-checkCirCMD = "./testfiles " + cirName
+checkCirCMD = "./testfiles ./Circuits/" + cirName
 os.system(checkCirCMD)
 
+# os.system("../")
+# os.system("ls")
 # Command to run the Circuit File
-runCirCMD = "./runtestgcparser " + cirName +" Server.txt Client.txt"
+runCirCMD = "./runtestgcparser ./Circuits/" + cirName +" ./Inputs/InputServer.txt ./Inputs/InputClient.txt"
 os.system(runCirCMD)
 #-------------------------------------------------------------------------------
 # Waits for Client and Server exchange

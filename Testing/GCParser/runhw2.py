479b4b94e1052b2c4cd5678aa81dfeb016234ca2
Mohammed Jasam <mnqnd@mst.edu>
2017-10-13 17:22:57 -0500

    Formatting the output

--- Testing/GCParser/runhw2.py
+++ Testing/GCParser/runhw2.py
@@ -2,10 +2,7 @@ import os
 import time
 import sys
 
-
-
 lenOfVector = int(sys.argv[1])
-# print("vex are"+ str(sys.argv[1]))
 #-------------------------------------------------------------------------------
 # Circuit name grabber
 try:
@@ -17,23 +14,24 @@ except:
     cirName = input()
 #-------------------------------------------------------------------------------
 # Inputs into the Client and Server
-for i in range(lenOfVector):
-    print("\nEnter the value for Client " + str(i))
+print("\nEnter elements separated by commas")
+s1 = input("Vector 1: ").split(",")
+s2 = input("Vector 2: ").split(",")
+s1 = [int(x) for x in s1[:lenOfVector]]
+s2 = [int(x) for x in s2[:lenOfVector]]
+
+for i in range(len(s1)):
     with open("./Inputs/InputClient.txt", "a") as text_file:
-        a=int(input())
-        print("a" + str(i) + " %d" %a, file=text_file)
+        print("a" + str(i) + " %d" %s1[i], file=text_file)
 
-    print("\nEnter the value for Server "+ str(i))
     with open("./Inputs/InputServer.txt", "a") as text_file:
-        b=int(input())
-        print("b" + str(i) + " %d" %b, file=text_file)
+        print("b" + str(i) + " %d" %s2[i], file=text_file)
 
-print("Enter the threshold")
 with open("./Inputs/InputClient.txt", "a") as text_file:
-    a = int(input())
+    a = int(input("Enter the threshold: "))
     print("t " + str(a), file = text_file)
 
-print("\n\n")
+print("\n")
 #-------------------------------------------------------------------------------
 # Command to check the Circuit file
 checkCirCMD = "./testfiles ./Circuits/" + cirName
@@ -47,7 +45,7 @@ os.system(runCirCMD)
 time.sleep(2)
 #-------------------------------------------------------------------------------
 # Displays their results
-print("\n\n")
+print("\n")
 print("Client output")
 os.system("cat ./results/siclient* | grep Product")
 print("\nServer output")

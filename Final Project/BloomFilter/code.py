ed31dd26d5032e216ffa78f76287dea0e1268dda
Mohammed Jasam <mnqnd@mst.edu>
2017-11-11 18:28:46 -0600

    Bloom Filter created!

--- Final Project/BloomFilter/code.py
+++ Final Project/BloomFilter/code.py
@@ -5,69 +5,83 @@ import random
 import hashlib
 
 SNum = [6, 7]
-unionSet = []
-S = []
-bitLen = 4
-preset = []
-appendFirst = []
-hashedAppendFirst = []
-randK = []
 
 
-# SHA-1 Hash function!
-def hashIt(s):
-    s = str.encode(s)
-    return hashlib.sha1(s).hexdigest()
+def genBloomFilter(l):
+    unionSet = []
+    S = []
+    bitLen = 4
+    preset = []
+    appendFirst = []
+    hashedAppendFirst = []
+    randK = []
+    # SHA-1 Hash function!
+    def hashIt(s):
+        s = str.encode(s)
+        return hashlib.sha1(s).hexdigest()
 
-# generate random values!
-for i in range(7):
-    randK.append(str(random.randint(1, 1000)))
+    # generate random values!
+    for i in range(7):
+        randK.append(str(random.randint(1, 1000)))
 
-# Convert to fixed length binary!
-string = '{0:0' + str(bitLen) + 'b}' # static BitLength
+    # Convert to fixed length binary!
+    string = '{0:0' + str(bitLen) + 'b}' # static BitLength
 
-for n in SNum:
-    bNum = string.format(n)
-    S.append(bNum)
+    for n in SNum:
+        bNum = string.format(n)
+        S.append(bNum)
 
-# Creating the prefix set!
-for element in S:
-    tree.genPrefix(element, preset)
+    # Creating the prefix set!
+    for element in S:
+        tree.genPrefix(element, preset)
 
-# Taking union of prefix set!
-unionSet = list(set(preset))
+    # Taking union of prefix set!
+    unionSet = list(set(preset))
 
-# Each node has a random value
-vr = random.randint(1, 1000)
+    # Each node has a random value
+    vr = random.randint(1, 1000)
+    print()
+    print("UnionSet is: ")
+    print(unionSet)
+    print()
+    print("VR is:")
+    print(vr)
+    print()
 
-# print(unionSet)
-print()
+    # For every element in unionSet create the hashed values of the elements for some N iterations!
+    for prefix in unionSet:
+        l = []
+        hl = []
+        for index, num in enumerate(randK):
+            K = randK[index] + prefix
+            l.append(K)
+            # hk = hashIt(K)
+            # hl.append(hk)
+            hK = hashIt(K) + str(vr)
+            hhKvr = hashIt(hK)
+            hl.append(hhKvr)
+        appendFirst.append(l)
+        hashedAppendFirst.append(hl)
 
-# For every element in unionSet create the hashed values of the elements for some N iterations!
-for prefix in unionSet:
-    l = []
-    hl = []
-    for index, num in enumerate(randK):
-        K = randK[index] + prefix
-        l.append(K)
-        hk = hashIt(K)
-        # hK = hashIt(K) + str(vr)
-        # hhKvr = hashIt(hk)
-        hl.append(hk)
-    appendFirst.append(l)
-    hashedAppendFirst.append(hl)
+    print("The random K are:")
+    print(randK)
+    print()
 
-print("The random K are:")
-print(randK)
-print()
+    # m value to create the bloomfilter
+    m = 10 * len(unionSet)
 
+    bloomFilter = []
+    for i in range(m):
+        bloomFilter.append(0)
+    print(bloomFilter)
 
-# for x in range(len(unionSet)):
-#     print(unionSet[x], appendFirst[x], hashedAppendFirst[x])
-# for x in appendFirst:
-#     print(x)
-#
-for x in hashedAppendFirst:
-    print(x)
+    for sett in hashedAppendFirst:
+        for x in sett:
+            a = int(x, 16)
+            i = a % m
+            # print(i)
+            bloomFilter[i - 1] = 1
+    print(bloomFilter)
 
-# tree.getTree()
+    return bloomFilter
+    # tree.getTree()
